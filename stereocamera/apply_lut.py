d90299291401a9e333cb061388651a9f134c4a80
Lyndon Hill <emptystate@yahoo.co.uk>
2026-09-14 20:59:43 +0100

    Add type hints, fix formatting, fix missing file

--- stereocamera/apply_lut.py
+++ stereocamera/apply_lut.py
@@ -17,66 +17,70 @@ LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def process_lut(input_image, lut):
-  """
-  Apply a LUT to an image using trilinear interpolation
+def process_lut(
+    input_image: cv2.typing.MatLike, lut: colour.LUT3D
+) -> cv2.typing.MatLike:
+    """
+    Apply a LUT to an image using trilinear interpolation
 
-  Params
-    input_image:  the input image
-    lut:  the look up table
+    Params
+      input_image:  the input image
+      lut:  the look up table
 
-  Return
-    processed image
-  """
+    Return
+      processed image
+    """
 
-  image_float = input_image.astype(np.float32) / 255.0
-  return lut.apply(image_float)
+    image_float = input_image.astype(np.float32) / 255.0
+    return lut.apply(image_float)
 
 
-def process_images(input_dir, lut_path, output_dir):
-  """
-  Process images
+def process_images(input_dir: Path, lut_path: Path, output_dir: Path):
+    """
+    Process images
 
-  Params
-    input_dir:  input directory with image files
-    lut_path:  path to the LUT file (.cube)
-    output_dir:  output directory for processed files
-  """
+    Params
+      input_dir:  input directory with image files
+      lut_path:  path to the LUT file (.cube)
+      output_dir:  output directory for processed files
+    """
 
-  if output_dir == input_dir:
-    raise ValueError(f"Output directory cannot be same as input directory {str(input_dir)}")
+    if output_dir == input_dir:
+        raise ValueError(
+            f"Output directory cannot be same as input directory {str(input_dir)}"
+        )
 
-  # Read LUT
-  lut = colour.io.read_LUT(lut_path)
+    # Read LUT
+    lut = colour.io.read_LUT(lut_path)
 
-  # Process all files in input directory; work in RGB
-  for filename in input_dir.iterdir():
-    extension = filename.suffix.lower()
-    if extension == ".jpg" or extension == ".png":
-      LOG.info(f"Processing {filename}")
-      # Load image
-      input_img = cv2.imread(filename)
-      input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
+    # Process all files in input directory; work in RGB
+    for filename in input_dir.iterdir():
+        extension = filename.suffix.lower()
+        if extension == ".jpg" or extension == ".png":
+            LOG.info(f"Processing {filename}")
+            # Load image
+            input_img = cv2.imread(filename)
+            input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
 
-      # Process
-      output_float = process_lut(input_img, lut)
-      output_img = (np.clip(output_float, 0.0, 1.0) * 255.0).astype(np.uint8)
-      output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
+            # Process
+            output_float = process_lut(input_img, lut)
+            output_img = (np.clip(output_float, 0.0, 1.0) * 255.0).astype(np.uint8)
+            output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
 
-      # Save
-      image_file = Path(filename).name
-      LOG.info(f"Writing {str(output_dir / image_file)}")
-      cv2.imwrite(str(output_dir / image_file), output_img)
+            # Save
+            image_file = Path(filename).name
+            LOG.info(f"Writing {str(output_dir / image_file)}")
+            cv2.imwrite(str(output_dir / image_file), output_img)
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(
-                      prog='correct_images',
-                      description='Apply a 3D LUT to a directory of images')
+    parser = argparse.ArgumentParser(
+        prog="correct_images", description="Apply a 3D LUT to a directory of images"
+    )
 
-  parser.add_argument("--input", type=Path, help="Input directory")
-  parser.add_argument("--lut", type=Path, help="LUT cube file")
-  parser.add_argument("--output", type=Path, help="Output directory")
+    parser.add_argument("--input", type=Path, help="Input directory")
+    parser.add_argument("--lut", type=Path, help="LUT cube file")
+    parser.add_argument("--output", type=Path, help="Output directory")
 
-  args = parser.parse_args()
-  process_images(args.input, args.lut, args.output)
+    args = parser.parse_args()
+    process_images(args.input, args.lut, args.output)
--- stereocamera/stsync.py
+++ stereocamera/stsync.py
@@ -29,6 +29,7 @@ import argparse
 import logging
 import subprocess
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
 from scipy import signal
@@ -42,194 +43,243 @@ LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def setup_output(output_dir):
-  """
-  Create output directory structure:
-    output directory and left and right subdirectories
+def setup_output(output_dir: Path):
+    """
+    Create output directory structure:
+      output directory and left and right subdirectories
 
-  Params
-    output_dir:  name of output directory
-  """
+    Params
+      output_dir:  name of output directory
+    """
 
-  for folder in ["left", "right"]:
-    (output_dir / folder).mkdir(parents=True, exist_ok=True)
-
-
-def get_video_fps(video):
-  """
-  Get frame rate of video file
-
-  Params
-    video:  path to video
-
-  Return
-    frame rate
-  """
-
-  cmd = [
-      'ffprobe', '-v', 'error',
-      '-select_streams', 'v:0',
-      '-show_entries', 'stream=avg_frame_rate',
-      '-of', 'default=noprint_wrappers=1:nokey=1', str(video)
-  ]
-  result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-
-  num, den = map(float, result.stdout.strip().split("/"))
-  return num / den
+    for folder in ["left", "right"]:
+        (output_dir / folder).mkdir(parents=True, exist_ok=True)
 
 
-def extract_audio(file_path):
-  """
-  Extract mono audio from a video file using FFmpeg.
+def get_video_fps(video: Path) -> float:
+    """
+    Get frame rate of video file
 
-  Params
-    file_path: path to video file that contains audio
+    Params
+      video:  path to video
 
-  Return:
-    audio as numpy array
-  """
+    Return
+      frame rate
+    """
 
-  cmd = [
-      "ffmpeg", "-hide_banner", "-loglevel", "error",
-      "-i", str(file_path),
-      "-t", str(ANALYSIS_DURATION),
-      "-ac", "1",                    # mono
-      "-ar", str(SAMPLE_RATE),       # sample rate
-      "-f", "f32le",                 # 32-bit float PCM
-      "-"                            # output to stdout
-  ]
-  result = subprocess.run(cmd, capture_output=True, check=True)
+    cmd = [
+        "ffprobe",
+        "-v",
+        "error",
+        "-select_streams",
+        "v:0",
+        "-show_entries",
+        "stream=avg_frame_rate",
+        "-of",
+        "default=noprint_wrappers=1:nokey=1",
+        str(video),
+    ]
+    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
 
-  # Convert raw bytes to numpy array
-  audio = np.frombuffer(result.stdout, dtype=np.float32)
-  return audio
-
-
-def plot_correlation(raw_left, raw_right):
-  """
-  Plot the cross-correlation in the region of the peak correlation
-
-  Params
-    raw_left:  raw audio samples from left video
-    raw_right:  raw audio samples from right video
-  """
-
-  corr = signal.correlate(raw_left, raw_right, mode='full')
-  lags = signal.correlation_lags(len(raw_left), len(raw_right), mode='full')
-
-  peak = np.argmax(corr)
-  plt.plot(lags[peak-2000:peak+2000], corr[peak-2000:peak+2000])
-  plt.show()
+    num, den = map(float, result.stdout.strip().split("/"))
+    return num / den
 
 
-def find_offset(raw_left, raw_right):
-  """
-  Find offset between audio using cross-correlation.
-  Positive offset means that the left video starts before the right.
+def extract_audio(file_path: Path) -> np.ndarray:
+    """
+    Extract mono audio from a video file using FFmpeg.
 
-  Params
-    raw_left:  raw audio samples from left video
-    raw_right:  raw audio samples from right video
+    Params
+      file_path: path to video file that contains audio
 
-  Return:
-    offset (seconds) and confidence of result
-  """
+    Return:
+      audio as numpy array
+    """
 
-  correlation = signal.correlate(raw_left, raw_right, mode='full')
-  peak_index = np.argmax(correlation)
+    cmd = [
+        "ffmpeg",
+        "-hide_banner",
+        "-loglevel",
+        "error",
+        "-i",
+        str(file_path),
+        "-t",
+        str(ANALYSIS_DURATION),
+        "-ac",
+        "1",  # mono
+        "-ar",
+        str(SAMPLE_RATE),  # sample rate
+        "-f",
+        "f32le",  # 32-bit float PCM
+        "-",  # output to stdout
+    ]
+    result = subprocess.run(cmd, capture_output=True, check=True)
 
-  lag_samples = peak_index - (len(raw_right) - 1)
-  offset = lag_samples / SAMPLE_RATE
-
-  confidence = np.abs(correlation[peak_index]) / np.mean(np.abs(correlation))
-  return offset, confidence
-
-
-def extract_frames(video_file, frame_rate, offset, output_dir, lut_file = None):
-  """
-  Extract all frames from a video and save as image files
-
-  Params
-    video_file:  input video file
-    frame_rate:  frame rate of video (assumed constant)
-    offset:  offset in seconds before starting decoding
-    output_dir:  path to write output frames
-  """
-
-  cmd = [
-      "ffmpeg", "-hide_banner", "-loglevel", "error",
-      "-i", str(video_file),
-      "-r", str(frame_rate),
-      "-ss", str(offset),
-      "-start_number", "1",
-      str(output_dir / "frame%05d.png")
-  ]
-
-  if lut_file is not None:
-    cmd.insert(6, "-vf")
-    cmd.insert(7, f"lut3d={str(lut_file)}")
-
-  subprocess.run(cmd, check=True)
+    # Convert raw bytes to numpy array
+    audio = np.frombuffer(result.stdout, dtype=np.float32)
+    return audio
 
 
-def audio_sync(left, right, trimleft, trimright, lut, output):
-  """
-  Extract audio and use it to sync videos
+def plot_correlation(raw_left: np.ndarray, raw_right: np.ndarray):
+    """
+    Plot the cross-correlation in the region of the peak correlation
 
-  Params
-    left: left video file
-    right: right video file
-    trimleft: additional frames for offset
-    trimright: additional frames for offset
-    output: path to directory for synchronised output frames
-  """
+    Params
+      raw_left:  raw audio samples from left video
+      raw_right:  raw audio samples from right video
+    """
 
-  setup_output(output)
+    corr = signal.correlate(raw_left, raw_right, mode="full")
+    lags = signal.correlation_lags(len(raw_left), len(raw_right), mode="full")
 
-  # Extract audio from both videos
-  left_audio = extract_audio(left)
-  right_audio = extract_audio(right)
+    peak = np.argmax(corr)
+    plt.plot(lags[peak - 2000 : peak + 2000], corr[peak - 2000 : peak + 2000])
+    plt.show()
 
-  # plot_correlation(left_audio, right_audio)
 
-  # Use cross-correlation to estimate offset
-  offset, confidence = find_offset(left_audio, right_audio)
+def find_offset(raw_left: np.ndarray, raw_right: np.ndarray) -> Tuple[float, float]:
+    """
+    Find offset between audio using cross-correlation.
+    Positive offset means that the left video starts before the right.
 
-  LOG.info(f"{offset}s offset to the right video, confidence {confidence}")
+    Params
+      raw_left:  raw audio samples from left video
+      raw_right:  raw audio samples from right video
 
-  if confidence < 40:
-    LOG.warning("Low confidence warning!")
+    Return:
+      offset (seconds) and confidence of result
+    """
 
-  fps = get_video_fps(left)
+    correlation = signal.correlate(raw_left, raw_right, mode="full")
+    peak_index = np.argmax(correlation)
 
-  # Round offset; FFmpeg will output next frame after given time
-  new_offset_frames = np.floor(np.abs(offset)*fps-0.5)
-  new_offset = new_offset_frames/fps
+    lag_samples = peak_index - (len(raw_right) - 1)
+    offset = lag_samples / SAMPLE_RATE
 
-  if offset >= 0:
-    LOG.info("Extracting frame from left video; left recording first")
-    trim3 = trimleft*(1.0/fps)
-    extract_frames(left, fps, max(0, new_offset-trim3), output / "left")
-    LOG.info("Extracting frames from right video")
-    extract_frames(right, fps, 0, output / "right", lut)
-  else:
-    LOG.info("Extracting frames from left video, right recording first")
-    trim3 = trimright*(1.0/fps)
-    extract_frames(left, fps, 0, output / "left")
-    LOG.info("Extracting frames from right video")
-    extract_frames(right, fps, max(0, new_offset+trim3), output / "right", lut)
+    confidence = np.abs(correlation[peak_index]) / np.mean(np.abs(correlation))
+    return offset, confidence
+
+
+def extract_frames(
+    video_file: Path,
+    frame_rate: float,
+    offset: float,
+    output_dir: Path,
+    lut_file: Path | None = None,
+):
+    """
+    Extract all frames from a video and save as image files
+
+    Params
+      video_file:  input video file
+      frame_rate:  frame rate of video (assumed constant)
+      offset:  offset in seconds before starting decoding
+      output_dir:  path to write output frames
+    """
+
+    cmd = [
+        "ffmpeg",
+        "-hide_banner",
+        "-loglevel",
+        "error",
+        "-i",
+        str(video_file),
+        "-r",
+        str(frame_rate),
+        "-ss",
+        str(offset),
+        "-start_number",
+        "1",
+        str(output_dir / "frame%05d.png"),
+    ]
+
+    if lut_file is not None:
+        cmd.insert(6, "-vf")
+        cmd.insert(7, f"lut3d={str(lut_file)}")
+
+    subprocess.run(cmd, check=True)
+
+
+def audio_sync(
+    left: Path, right: Path, trimleft: int, trimright: int, lut: Path, output: Path
+):
+    """
+    Extract audio and use it to sync videos
+
+    Params
+      left:  left video file
+      right:  right video file
+      trimleft:  additional frames for offset
+      trimright:  additional frames for offset
+      lut:  path to LUT file
+      output:  path to directory for synchronised output frames
+    """
+
+    setup_output(output)
+
+    # Extract audio from both videos
+    left_audio = extract_audio(left)
+    right_audio = extract_audio(right)
+
+    # plot_correlation(left_audio, right_audio)
+
+    # Use cross-correlation to estimate offset
+    offset, confidence = find_offset(left_audio, right_audio)
+
+    LOG.info(f"{offset}s offset to the right video, confidence {confidence}")
+
+    if confidence < 40:
+        LOG.warning("Low confidence warning!")
+
+    fps = get_video_fps(left)
+
+    # Round offset; FFmpeg will output next frame after given time
+    new_offset_frames = np.floor(np.abs(offset) * fps - 0.5)
+    new_offset = new_offset_frames / fps
+
+    if offset >= 0:
+        LOG.info("Extracting frame from left video; left recording first")
+        trim3 = trimleft * (1.0 / fps)
+        extract_frames(left, fps, max(0, new_offset - trim3), output / "left")
+        LOG.info("Extracting frames from right video")
+        extract_frames(right, fps, 0, output / "right", lut)
+    else:
+        LOG.info("Extracting frames from left video, right recording first")
+        trim3 = trimright * (1.0 / fps)
+        extract_frames(left, fps, 0, output / "left")
+        LOG.info("Extracting frames from right video")
+        extract_frames(right, fps, max(0, new_offset + trim3), output / "right", lut)
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Synchronise two videos using audio.")
-  parser.add_argument("--left", type=Path, help="Path to left video file", required=True)
-  parser.add_argument("--right", type=Path, help="Path to right video file, required=True")
-  parser.add_argument("--output", type=Path, help="Path for output frames, required=True")
-  parser.add_argument("--trimleft", type=int, default=2, help="Trim amount if left recording started first")
-  parser.add_argument("--trimright", type=int, default=4, help="Trim amount if right recording started first")
-  parser.add_argument("--lut", type=Path, help="Look Up Table for colour correction of right camera")
+    parser = argparse.ArgumentParser(description="Synchronise two videos using audio.")
+    parser.add_argument(
+        "--left", type=Path, help="Path to left video file", required=True
+    )
+    parser.add_argument(
+        "--right", type=Path, help="Path to right video file, required=True"
+    )
+    parser.add_argument(
+        "--output", type=Path, help="Path for output frames, required=True"
+    )
+    parser.add_argument(
+        "--trimleft",
+        type=int,
+        default=2,
+        help="Trim amount if left recording started first",
+    )
+    parser.add_argument(
+        "--trimright",
+        type=int,
+        default=4,
+        help="Trim amount if right recording started first",
+    )
+    parser.add_argument(
+        "--lut", type=Path, help="Look Up Table for colour correction of right camera"
+    )
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  audio_sync(args.left, args.right, args.trimleft, args.trimright,
-             args.lut, args.output)
+    audio_sync(
+        args.left, args.right, args.trimleft, args.trimright, args.lut, args.output
+    )
